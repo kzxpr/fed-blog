33bbd503e468408b33d6f4a410cb2cd2f13d89f6
kzxpr <kzxpr@protonmail.com>
2023-04-07 17:33:25 +0200

    Changes to submodule

--- myInstance.js
+++
@@ -1,9 +0,0 @@
-const MyClass = require("./myClass")
-
-const instance = new MyClass("Cornelius")
-
-instance.eventHandler.on("test", () => {
-    console.log("Dette sker!")
-})
-
-instance.myName();
